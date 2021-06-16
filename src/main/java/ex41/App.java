3d2bc085f469b2a02c43583a4365b54e850fbb9b
fshofiquel <fshofique@gmail.com>
2021-06-16 14:17:46 -0400

    first commit

--- src/main/java/ex41/App.java
+++ src/main/java/ex41/App.java
@@ -1,5 +1,46 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 3 Solution
+ *  Copyright 2021 Fazlur Shofiquel
+ */
 package ex41;
+
+import java.io.*;
 
 public class App
 {
+    public static sortedList newList = new sortedList();
+    public static String names;
+
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader iF = new BufferedReader(new FileReader("src/main/java/ex41/exercise41_input.txt"));
+        BufferedWriter oF = new BufferedWriter(new FileWriter("src/main/java/ex41/exercise41_output.txt"));
+
+        newList.storeNames(iF, oF, names);
+        System.out.print("Operation completed");
+        iF.close();
+        oF.close();
+
+    }
 }
+
+class sortedList
+{
+    public void storeNames(BufferedReader iF, BufferedWriter oF, String names) throws IOException
+    {
+        while ((names = iF.readLine()) != null)
+        {
+            oF.write(names);
+        }
+
+    }
+
+    public void sortNames()
+    {
+    }
+
+    public void outputToFile()
+    {
+    }
+}
+
