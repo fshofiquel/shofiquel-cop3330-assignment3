254d5ad5ef9ef6f1eab36d9abae0d3bf3bfea913
fshofiquel <fshofique@gmail.com>
2021-06-20 20:48:02 -0400

    Add pseudo code to 42 and update plantUML

--- src/main/java/ex45/App.java
+++ src/main/java/ex45/App.java
@@ -8,9 +8,16 @@ public class App
 	public static replacer replacer = new replacer();
 	public static void main(String[] args) throws IOException
 	{
+		String file = "src/main/java/ex45/";
+		String prompt;
+		Scanner prompter = new Scanner(System.in);
 		Scanner inputFile = new Scanner(new File("src/main/java/ex45/exercise45_input.txt"));
-		BufferedWriter outputFile = new BufferedWriter
-				(new FileWriter("src/main/java/ex45/exercise45_output.txt"));
+
+
+		System.out.print("Provide file name: ");
+		prompt = prompter.nextLine();
+		File fileOut = new File(file+prompt+"/");
+		BufferedWriter outputFile = new BufferedWriter(new FileWriter(fileOut));
 
 		replacer.storeAndReplaceString(inputFile, outputFile);
 		inputFile.close();
